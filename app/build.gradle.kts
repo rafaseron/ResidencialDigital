f920239f6310194d58979945593720c7fe68d0f5
Rafael Lui Seron de Paula <63885470+rafaseron@users.noreply.github.com>
2025-05-12 17:31:30 -0400

    project init

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "br.com.rafaseron.residencialdigital"
+    compileSdk = 35
+
+    viewBinding {
+        enable = true
+    }
+
+    defaultConfig {
+        applicationId = "br.com.rafaseron.residencialdigital"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.ktor.client.core)
+    implementation(libs.koin.android)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.okhttp)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
